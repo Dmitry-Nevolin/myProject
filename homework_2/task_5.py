cb9f49bc55a5a64b41090075f02a49596290c2d1
Dmitry Nevolin <nevolin.dmitrij@mail.ru>
2022-02-22 20:41:36 +0300

    Homework 2 File "task_5" was updated

--- homework_2/task_5.py
+++ homework_2/task_5.py
@@ -3,12 +3,17 @@
 # Если в рейтинге существуют элементы с одинаковыми значениями,
 # то новый элемент с тем же значением должен разместиться после них.
 
-my_list = [6, 3, 2, 1]
+my_list = [7, 4, 2, 1]
 number = int(input("Введите цифру: "))
+i = 0
 if number in my_list:
     my_list.insert(my_list.index(number) + 1, number)
 else:
-    my_list.append(number)
-    my_list.sort()
-    my_list.reverse()
-print(my_list)
+    while True:
+        if number > my_list[i]:
+            my_list.insert(i, number)
+            break
+        elif number < my_list[i]:
+            i += 1
+
+print(f"New list: {my_list}")
